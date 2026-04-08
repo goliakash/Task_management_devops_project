d341ac905c75716d113035d56830b9906d3b5acd
goliakash <goliakash710@gmail.com>
2026-04-08 10:58:22 +0000

    fixed some errors and some stuff

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -24,6 +24,10 @@ export function clearToken() {
   localStorage.removeItem(TOKEN_KEY)
 }
 
+export function getToken() {
+  return localStorage.getItem(TOKEN_KEY)
+}
+
 export async function login(credentials) {
   const response = await api.post('/auth/login', credentials)
 
